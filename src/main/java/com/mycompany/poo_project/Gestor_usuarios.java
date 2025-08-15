14a04de7935d2519bab56ced72cf8b3d419ea3ae
andrea-lc <andreadelr.lc@gmail.com>
2025-08-15 18:26:39 -0500

    Semana 1 s1: Avance proyecto

---
+++ src/main/java/com/mycompany/poo_project/Gestor_usuarios.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_project;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+class Gestor_usuarios {
+    private static final String Ruta_archivo = "usuarios.txt";
+    
+    public void registrar_usuario (String usuario,String contraseña){
+        try(BufferedWriter bw= new BufferedWriter (new FileWriter(Ruta_archivo,true)) ){
+            bw.write(usuario + "," + contraseña);
+            bw.newLine();
+            System.out.println("Usuario agregado correctamente.");
+        } catch (IOException ex) {
+            System.out.println("Error al leer usuarios.txt"+ex);
+        }      
+    }
+    
+       public boolean login(String usuario, String contraseña) {
+        try (BufferedReader br = new BufferedReader(new FileReader(Ruta_archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split("\\,");
+                if (datos.length >= 2) {
+                    String validarUsuario = datos[0];
+                    String validarUser = datos[1];
+                    if (validarUsuario.equals(usuario) && validarUser.equals(contraseña)) {
+                        return true;
+                    }
+                    System.out.println("Ingresando...");
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Error leyendo archivo: " + ex);
+        }
+        return false;
+    }
+       
+     public List<String[]> leer_usuario() {
+        List<String[]> lista = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Ruta_archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split("\\,");
+                lista.add(datos);
+            }
+        } catch (IOException ex) {
+            System.out.println("Error al leer usuarios.txt"+ex);
+        }
+        return lista;
+    }
+}
+
---
+++ src/main/java/com/mycompany/poo_project/POO_project.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class POO_project {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner (System.in);
+        Gestor_usuarios gu=new Gestor_usuarios(); 
+        int opcion;
+        do {
+            System.out.println("""
+                               -------Praderas del Adiós (cementerio)------
+                               1) Iniciar sesion
+                               2) Registrar nuevo Usuario
+                               3) Salir 
+                               --------------------------------------------""");    
+            opcion=scanner.nextInt(); 
+            switch (opcion){
+                case 1:System.out.println("");
+                    System.out.println("Nombre de Usuario: ");
+                    String user_name=scanner.nextLine();
+                    scanner.nextLine();
+                    System.out.println("C2onstraseña: ");
+                    String contraseña=scanner.nextLine();
+                    gu.login(user_name, contraseña);
+                    
+                case 2: 
+                    System.out.println("Nombre de Usuario: ");
+                    String user_name1=scanner.nextLine();
+                    scanner.nextLine();
+                    System.out.println("Cree una constraseña: ");
+                    String contraseña1=scanner.nextLine();   
+                    gu.registrar_usuario(user_name1, contraseña1);
+                case 3:                  
+                    break;              
+                default: System.out.println("Ingrese una opcion valida");
+            }
+        }while (opcion!=0);        
+    }
+}
