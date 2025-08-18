930efd61a7a61f809bcf692cbb9e2d9d96ec9346
andrea-lc <andreadelr.lc@gmail.com>
2025-08-17 21:22:08 -0500

    Semana 1 s2: Avance del proyecto

--- src/main/java/com/mycompany/poo_project/Gestor_usuarios.java
+++ src/main/java/com/mycompany/poo_project/Gestor_usuarios.java
@@ -9,8 +9,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  *
@@ -19,7 +18,6 @@ import java.util.List;
 class Gestor_usuarios {
     //ATRIBUTOS
     private static final String Ruta_archivo = "usuarios.txt";
-    
     //METODOS
     public void registrar_usuario (String usuario,String contraseña){
         try(BufferedWriter bw= new BufferedWriter (new FileWriter(Ruta_archivo,true)) ){
@@ -42,27 +40,12 @@ class Gestor_usuarios {
                     if (validarUsuario.equals(usuario) && validarUser.equals(contraseña)) {
                         return true;
                     }
-                    System.out.println("Ingresando...");
                 }
             }
         } catch (IOException ex) {
             System.out.println("Error leyendo archivo: " + ex);
         }
         return false;
-    }
-       
-     public List<String[]> leer_usuario() {
-        List<String[]> lista = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(Ruta_archivo))) {
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] datos = linea.split("\\,");
-                lista.add(datos);
-            }
-        } catch (IOException ex) {
-            System.out.println("Error al leer usuarios.txt"+ex);
-        }
-        return lista;
-    }
+    }     
 }
 
--- src/main/java/com/mycompany/poo_project/POO_project.java
+++ src/main/java/com/mycompany/poo_project/POO_project.java
@@ -11,39 +11,58 @@ import java.util.Scanner;
  * @author admin
  */
 public class POO_project {
-
     public static void main(String[] args) {
         Scanner scanner=new Scanner (System.in);
         Gestor_usuarios gu=new Gestor_usuarios(); 
         int opcion;
         do {
             System.out.println("""
-                               -------Praderas del Adiós (cementerio)------
+                               ------------------Cat Haven------------------
                                1) Iniciar sesion
                                2) Registrar nuevo Usuario
                                3) Salir 
                                --------------------------------------------""");    
             opcion=scanner.nextInt(); 
-            switch (opcion){
-                case 1:System.out.println("");
-                    System.out.println("Nombre de Usuario: ");
-                    String user_name=scanner.nextLine();
-                    scanner.nextLine();
-                    System.out.println("Contraseña: ");
-                    String contraseña=scanner.nextLine();
-                    gu.login(user_name, contraseña);
-                    
-                case 2: 
+            scanner.nextLine();
+            switch (                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 opcion){
+                case 1 -> { 
+                    boolean logincorrecto=false;
+                    boolean intentardenuevo=true;
+                    do{
+                        System.out.println("Nombre de Usuario: ");
+                        String user_name=scanner.nextLine();
+                        System.out.println("Contraseña: ");
+                        String contraseña=scanner.nextLine();
+                        if (gu.login(user_name, contraseña)) {
+                            System.out.println("Ingresando...");
+                            logincorrecto = true;
+                        } else {
+                            System.out.println("Usuario o contraseña incorrectos.");
+                            System.out.print("Desea intentar de nuevo? (si/no): ");
+                            String volvermenu = scanner.nextLine();
+                            
+                            if (volvermenu.equalsIgnoreCase("no")) {
+                                intentardenuevo = false;
+                                System.out.println("Regresando al menú principal...");
+                            }
+                        }
+                    } while (!logincorrecto && intentardenuevo);
+                    break;
+                }
+                case 2 -> { 
                     System.out.println("Nombre de Usuario: ");
                     String user_name1=scanner.nextLine();
-                    scanner.nextLine();
+   
                     System.out.println("Cree una constraseña: ");
                     String contraseña1=scanner.nextLine();   
                     gu.registrar_usuario(user_name1, contraseña1);
-                case 3:                  
-                    break;              
-                default: System.out.println("Ingrese una opcion valida");
+                    break;
+                }
+                case 3 -> {
+                    break;
+                }
+                default -> System.out.println("Ingrese una opcion valida");
             }
-        }while (opcion!=0);        
+        }while (opcion!=3);        
     }
 }
