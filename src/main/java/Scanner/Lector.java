6a66fc7fc02bf7e576a592d0009a99f01c77afe5
andrea-lc <andreadelr.lc@gmail.com>
2025-09-13 21:23:51 -0500

    Avance proyecto: scanner

---
+++ src/main/java/Scanner/Lector.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Scanner;
+
+import java.util.InputMismatchException;
+
+/**
+ *
+ * @author admin
+ */
+public class Lector {
+    private final java.util.Scanner scanner = new java.util.Scanner (System.in);
+    
+    public int LeerEntero (){
+        while (true){
+            try {
+                return scanner.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("Ingrese un numero valido: ");
+                scanner.nextLine();
+            }
+        }
+    }
+    public String LeerString () {
+     while (true){
+         String salida=scanner.nextLine();
+            if (!salida.isEmpty()){
+              return salida.trim();
+            } else {
+                System.out.println("Ingrese un dato valido: ");
+            }
+        }
+    }
+}
